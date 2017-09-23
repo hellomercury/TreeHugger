fa058b4d0d28f48b6cd5e1029f0ed8642255a239
Ian Witecki <iwitecki@trinity.edu>
2017-09-22 21:25:36 -0500

    First Commit. Working on movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody rgb;
+    public float floatSpeed = 5f;
+    private float zmove = 2f;
+    private Vector3 depthMove = new Vector3(0, Mathf.Sin(Mathf.PI / 6), Mathf.Cos(Mathf.PI / 6));
+    private Vector3 horizontalMove = new Vector3(1,0,0);
+    private Vector3 jumpMove = new Vector3(0, 1, 0);
+    private float speed = 4f;
+
+    // Use this for initialization
+    void Start () {
+        rgb = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inX = Input.GetAxis("Horizontal");
+        float inZ = Input.GetAxis("Z");
+        float inY = Input.GetAxis("Jump");
+        rgb.AddForce(speed * (inX * horizontalMove));
+        rgb.AddForce(speed * (inZ * depthMove));
+        rgb.AddForce(speed * (inY * jumpMove));
+    }
+}
